4f2ff2138ca45c2756f951fd71f80acc1328f117
Amit Ben Amara <amit444333@gmail.com>
2024-07-20 13:51:17 +0300

    Deleted cheat.txt (no need), added a templates folder, added a static folder, redisogned ourwebsite

--- flaskWeb.py
+++ flaskWeb.py
@@ -1,17 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": 'Amit Ben Amara',
+        "title": 'First post',
+        "content": 'OneDrive sucks dick',
+        "date_posted": '20.07.2024' 
+    },
+    {
+        "author": 'Amit Ben Amara',
+        "title": 'Second post',
+        "content": 'OneDrive sucks sooooo much dick',
+        "date_posted": '21.07.2024' 
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello, Benyamin!!!!! FUCKKKKE</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>Its a me</h1>"
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
