ec377d96f1a61b16982144c8ed1ea7a9712b8bf8
Amit Ben Amara <amit444333@gmail.com>
2024-07-21 13:16:19 +0300

    Added some features, login and register page with cool stuff like validation and more

--- flaskWeb.py
+++ flaskWeb.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '03b5f57049b9881b93cd9ef9b6df7bb0'
 
 posts = [
     {
@@ -27,6 +28,26 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Hate Account Created For {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@hate.com' and form.password.data == 'hate':
+            flash('You have been hated in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Hatein Unsuccessful. Please check your Hate info', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
