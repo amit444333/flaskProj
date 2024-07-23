4b014d7010dd7be726f7a294b7060e4640d73923
Amit Ben Amara <amit444333@gmail.com>
2024-07-23 12:50:58 +0300

    Added the following features: Update Post, Delete Post, Post page, update Post page, managed differents routes, and made slight improvement to the site visuals, and add pip requirements

--- flaskWeb/routes.py
+++ flaskWeb/routes.py
@@ -1,30 +1,17 @@
 import os
 import secrets
 from PIL import Image
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskWeb import app, db, bcrypt
-from flaskWeb.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from flaskWeb.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskWeb.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 
-posts = [
-    {
-        "author": 'Amit Ben Amara',
-        "title": 'First post',
-        "content": 'OneDrive sucks dick',
-        "date_posted": '20.07.2024' 
-    },
-    {
-        "author": 'Amit Ben Amara',
-        "title": 'Second post',
-        "content": 'OneDrive sucks sooooo much dick',
-        "date_posted": '21.07.2024' 
-    }
-]
 
 @app.route("/")
 @app.route("/home")
 def home():
+    posts = Post.query.all()
     return render_template('home.html', posts=posts)
 
 
@@ -97,3 +84,49 @@ def account():
         form.email.data = current_user.email     
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+@app.route("/post/new", methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your Hate Post Has Been Created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form, legend='New Hate')
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your Hate Has Been Updated', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET': 
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='Update Post', form=form, legend='Update Hate')   
+
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your Hate Has Been Deleted','info')
+    return redirect(url_for('home'))    
