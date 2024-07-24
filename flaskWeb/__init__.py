65f44c501ebd3f3979f735fddc8c348da22d7247
Amit Ben Amara <amit444333@gmail.com>
2024-07-24 10:00:28 +0300

    Added User reset password throuh email, was a nightmare, environment variable caused me 3 hours of debug

--- flaskWeb/__init__.py
+++ flaskWeb/__init__.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '03b5f57049b9881b93cd9ef9b6df7bb0'
@@ -11,5 +13,21 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+
+app.config.update(
+MAIL_SERVER='smtp.gmail.com',
+MAIL_PORT='587',
+TESTING=False,
+MAIL_SUPPRESS_SEND = False,
+MAIL_DEBUG = True,
+MAIL_USE_TLS=True,
+# YOU HAVE TO SET YOU ENVIRONMENT VARIABLE TO BE YOUR EMAIL
+MAIL_USERNAME=os.environ['EMAIL_USER'],
+# YOU NEED TO HAVE 2 STEP-AUTH ON GOOGLE ACCOUNR, AND GET AN APP PASSSWORD AND PUT IT IN THE ENVIRONMENT VARIABLE
+MAIL_PASSWORD=os.environ['EMAIL_PASS']
+)
+
+mail = Mail(app)
 
 from flaskWeb import routes
