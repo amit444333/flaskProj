f4bb60f5b8baff9b0338d5a6394e233e7e78c680
Amit Ben Amara <amit444333@gmail.com>
2024-08-04 11:21:40 +0300

    Made an error page for 405 error

--- flaskWeb/errors/handlers.py
+++ flaskWeb/errors/handlers.py
@@ -3,6 +3,10 @@ from flask import Blueprint, render_template
 errors = Blueprint('erros', __name__)
 
 
+@errors.app_errorhandler(405)
+def error_405(error):
+    return render_template('errors/405.html'), 405
+
 @errors.app_errorhandler(404)
 def error_404(error):
     return render_template('errors/404.html'), 404
