e45761de3cd217ec15d0ade9156a441c4306bcd6
Amit Ben Amara <amit444333@gmail.com>
2024-08-06 01:08:19 +0300

    Fixed a bug that made the reset password not work

--- flaskWeb/models.py
+++ flaskWeb/models.py
@@ -16,15 +16,15 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
-    def get_reset_token(self, expires_sec=1800):
-        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
-        return s.dumps({'user_id': self.id}).decode('utf-8')
+    def get_reset_token(self):
+        s = Serializer(current_app.config['SECRET_KEY'], salt='1800')
+        return s.dumps({'user_id': self.id}, salt='1800')
     
     @staticmethod
     def verify_reset_token(token):
         s = Serializer(current_app.config['SECRET_KEY'])
         try:
-            user_id = s.loads(token)['user_id']
+            user_id = s.loads(token, salt='1800')['user_id']
         except:
             return None
         return User.query.get(user_id)    
